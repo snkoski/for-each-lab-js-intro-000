cfb71a07f2ce2d9899ea8cf0e6b71be469b04eaf
Shawn Koski <shawnnkoski@gmail.com>
2018-03-14 10:28:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 function iterativeLog(array) {
-  array.forEach(element => {console.log(`${Object.keys(array)}: ${Object.values(array)}`);)};
+  array.forEach(element => {console.log(`${Object.keys(array)}: ${Object.values(array)}`);});
 }
